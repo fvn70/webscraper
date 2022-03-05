6b6c82ff587dded192312a63e4e309c3769e39dd
fvn70 <fvn_reg@mail.ru>
2022-03-05 21:05:26 +0700

    Stage 2/5: The Beautiful Soup Ingredients

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def load_api(url):
     r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
@@ -6,8 +7,15 @@ def load_api(url):
 
 url = input("Input the URL:\n")
 
-r = load_api(url).json()
-if 'content' not in r.keys():
-    print(f"\nInvalid quote resource!")
+if 'title' not in url:
+    print('\nInvalid movie page!')
 else:
-    print(r['content'])
+    dic = {}
+    r = load_api(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    h_link = soup.find('h1')
+    dic['title'] = h_link.text
+    span_link = soup.find('span', 'GenresAndPlot__TextContainerBreakpointL-sc-cum89p-1 eqlIrG')
+    dic['description'] = span_link.text
+    print(dic)
+
