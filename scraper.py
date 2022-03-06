25f18686e9e706fd2e5b6b0b3e82922edab0ddc2
fvn70 <fvn_reg@mail.ru>
2022-03-06 21:22:10 +0700

    Stage 5/5: Soup, Sweet Soup

--- scraper.py
+++ scraper.py
@@ -9,31 +9,37 @@ def load_api(url):
     r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
     return r
 
-articles = []
-
-# url = input("Input the URL:\n")
-url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
+url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020"
 dom = "https://www.nature.com"
 art_list = []
 art_type = "News"
+page_num = 4
+art_type = "Research Highlight"
 
+page_num = int(input())
+art_type = input()
 
-r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-soup = BeautifulSoup(r.content, 'html.parser')
-arts = soup.find_all('article')
-for art in arts:
-    sp = art.find('span', {'class':'c-meta__type'})
-    # print(sp.text)
-    if sp.text == art_type:
-        a = art.find('a', {'data-track-action':'view article'})
-        href = a['href']
-        name = a.text
-        fn = f"{name.translate(str.maketrans('', '', string.punctuation)).replace(' ', '_')}.txt"
-        r = load_api(dom + href)
-        if r.status_code == 200:
-            soup_a = BeautifulSoup(r.content, 'html.parser')
-            body = soup_a.find('div', class_=re.compile(".*article.*body.*"))
-            with open(fn, 'wb') as file:
-                file.write(bytes(body.text.strip(), encoding='utf-8'))
-        art_list.append(fn)
+for n in range(page_num):
+    dir_name = f"Page_{n + 1}"
+    os.mkdir(dir_name)
+    url_p = f"{url}&page={n + 1}"
+    r = load_api(url_p)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    arts = soup.find_all('article')
+    for art in arts:
+        sp = art.find('span', {'class':'c-meta__type'})
+        if sp.text == art_type:
+            a = art.find('a', {'data-track-action':'view article'})
+            href = a['href']
+            name = a.text
+            fn = f"{name.translate(str.maketrans('', '', string.punctuation)).replace(' ', '_')}.txt"
+            r = load_api(dom + href)
+            if r.status_code == 200:
+                soup_a = BeautifulSoup(r.content, 'html.parser')
+                body = soup_a.find('div', class_=re.compile(".*article.*body.*"))
+                fn = os.path.join(dir_name, fn)
+                with open(fn, 'wb') as file:
+                    file.write(bytes(body.text.strip(), encoding='utf-8'))
+                art_list.append(fn)
 print(f"Saved articles ({len(art_list)}):  {art_list}")
